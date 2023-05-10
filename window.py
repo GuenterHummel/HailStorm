3515a243fe7fac960d124470b445c00b139070c7
Günter Hummel <guehummel@gmail.com>
2023-05-10 20:09:49 +0200

    update of settings and fixed formatting

--- window.py
+++ window.py
@@ -4,12 +4,12 @@ from tkinter import *
 # Die folgende Funktion soll ausgeführt werden, wenn
 # der Benutzer den Button anklickt
 def button_action():
-    anweisungs_label.config(text="Ich wurde geändert!")
+    instruction_label.config(text="Ich wurde geändert!")
 
 
 # Ein Fenster erstellen
 fenster = Tk()
-# Den Fenstertitle erstellen
+# Die Überschrift des Fensters erstellen
 fenster.title("Ich mache nun was.")
 fenster.minsize(500, 300)
 
@@ -17,15 +17,15 @@ fenster.minsize(500, 300)
 change_button = Button(fenster, text="Ändern", command=button_action)
 exit_button = Button(fenster, text="Beenden", command=fenster.quit)
 
-anweisungs_label = Label(fenster, text="Ich bin eine Anweisung:\n\
+instruction_label = Label(fenster, text="Ich bin eine Anweisung:\n\
 Klicke auf 'Ändern'.")
 
 info_label = Label(fenster, text="Ich bin eine Info:\n\
 Der Beenden Button schliesst das Programm.")
 
 # Nun fügen wir die Komponenten unserem Fenster
-# in der gwünschten Reihenfolge hinzu.
-anweisungs_label.pack()
+# in der gewünschten Reihenfolge hinzu.
+instruction_label.pack()
 change_button.pack()
 info_label.pack()
 exit_button.pack()
