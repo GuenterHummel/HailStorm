7e940c817e79f8951eeca7a0a4299a7d42e7f553
Guenter Hummel <guenter.hummel@sbs.co.at>
2026-07-09 17:01:24 +0200

    finished new bagles game

--- hailstorm/bagles.py
+++ hailstorm/bagles.py
@@ -18,7 +18,7 @@ By Al Sweigart al@inventwithpython.com
 I am thinking of a {}-digit number with no repeated digits.
 Try to guess what it is. Here are some clues:
 When I say:     That means:
-  Pico          One digit is correct but in teh wrong position.
+  Pico          One digit is correct but in the wrong position.
   Fermi         One digit is correct and in the right position.
   Bagels        No digit is correct.
   
@@ -32,6 +32,23 @@ clues would be Fermi Pico.'''.format(NUM_DIGITS))
         print (' You have {} guesses to get it.'.format (MAX_GUESSES))
 
         numGuesses = 1
+        while numGuesses <= MAX_GUESSES:
+            guess = ''
+            # Keep looping until they enter a valid guess:
+            while len(guess) != NUM_DIGITS or not guess.isdecimal():
+                print ('Guess #{}: '.format(numGuesses))
+                guess = input('> ')
+
+            clues = getClues(guess, secretNum)
+            print(clues)
+
+            numGuesses += 1
+
+            if guess == secretNum:
+                break # They're correct, so break out of this loop.
+            if numGuesses > MAX_GUESSES:
+                print('You ran out of guesses.')
+                print('The answer was {}.'.format(secretNum))
 
         # Ask player if they want to play again.
         print('Do you want to play again? (yes or no)')
@@ -49,6 +66,31 @@ def getSecretNum():
     for i in range(NUM_DIGITS):
         secretNum += str(numbers[i])
     return secretNum
+
+def getClues(guess, secretNum):
+    """Returns a string with pico, fermi, bagles, clues for a guess
+    and secret number pairs"""
+    if guess == secretNum:
+        return 'You got it!'
+
+    clues = []
+
+    for i in range(len(guess)):
+        if guess[i] == secretNum[i]:
+            # A correct digit is in the correct place.
+            clues.append('Fermi')
+        elif guess[i] in secretNum:
+            # A correct digit is in the incorrect place.
+            clues.append('Pico')
+
+    if len(clues) == 0:
+        return 'Bagels' # There are no correct digits at all.
+    else:
+        # Sort the clues into alphabetical order so their original order
+        # doesn't give information away.
+        clues.sort()
+        # Make a single string from the list of string clues.
+        return ' '.join(clues)
 
 
 # If the program is run (instead of imported), run the game:
